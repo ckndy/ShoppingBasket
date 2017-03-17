65f10d42e6b2c7b890ca7abf3f95e99e3816f7ce
ckndy <chris.kennedy@live.co.uk>
2017-03-17 22:20:22 +0000

    Added getters and setters

---
+++ shopping_basket_management/Customer.java
@@ -0,0 +1,26 @@
+package shopping_basket_management;
+
+public class Customer {
+  private String name;
+  private Boolean loyaltyCard;
+
+  public Customer(String name, Boolean loyaltyCard) {
+    this.name = name;
+    this.loyaltyCard = loyaltyCard;
+  }
+
+  public String getName() {
+
+  }
+
+  public String setName(String name) {
+
+  }
+
+  public boolean getLoyaltyCard();
+  
+}
+
+  public boolean setLoyaltyCard(boolean loyaltyCard) {
+
+}
