8916ff944819e8bb3cc38869956630ae3695e18e
ckndy <chris.kennedy@live.co.uk>
2017-03-17 22:01:31 +0000

    Added getters and setters

---
+++ shopping_basket_management_specs/BasketTest.java
@@ -0,0 +1,15 @@
+import shopping_basket_management;
+import static org.junit.Assert.assertEquals;
+import 
+
+public class BasketTest {
+
+  Basket basket;
+
+}
+
+@Before
+public void before() {
+
+  basket = new Basket();
+}
