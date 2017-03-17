350a1359ed3256d678988b0bd5a98a8e04ec1957
ckndy <chris.kennedy@live.co.uk>
2017-03-17 18:32:50 +0000

    Second commit

---
+++ shopping_basket_management/Item.java
@@ -0,0 +1,13 @@
+package shopping_basket_management;
+import 
+
+public class Item {
+
+  private String name;
+  private Double price;
+
+  public Item(String name, Double price) {
+    this.name = name;
+    this.price = price;
+  }
+}
